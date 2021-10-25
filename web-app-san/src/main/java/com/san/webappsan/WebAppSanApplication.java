1eaa691db25384b7e950dcc6513cb9b90aedb8c1
santeixeira <jbsanthiago@gmail.com>
2021-10-25 20:33:31 -0300

    first commit

---
+++ web-app-san/src/main/java/com/san/webappsan/WebAppSanApplication.java
@@ -0,0 +1,13 @@
+package com.san.webappsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppSanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppSanApplication.class, args);
+	}
+
+}
---
+++ web-app-san/src/test/java/com/san/webappsan/WebAppSanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.webappsan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppSanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
