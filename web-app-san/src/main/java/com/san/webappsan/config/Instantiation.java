5babaeb0f69d3b665e003efa5a080846e5670d16
santeixeira <jbsanthiago@gmail.com>
2021-10-26 14:53:24 -0300

    User POST reference

--- web-app-san/src/main/java/com/san/webappsan/config/Instantiation.java
+++ web-app-san/src/main/java/com/san/webappsan/config/Instantiation.java
@@ -6,6 +6,7 @@ import java.util.TimeZone;
 
 import com.san.webappsan.domain.Post;
 import com.san.webappsan.domain.User;
+import com.san.webappsan.dto.AuthorDTO;
 import com.san.webappsan.repository.PostRepository;
 import com.san.webappsan.repository.UserRepository;
 
@@ -35,11 +36,17 @@ public class Instantiation implements CommandLineRunner{
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User jack = new User(null, "Jack Johnson", "jack@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("26/10/2021"), "Kyrie Irving doesn't want to vaccinate.", "Shame on Kyrie, his selfishness attitude is damaging his team, Brooklyn Nets, to get any chance of going to play-offs this season.", alex);
-        Post post2 = new Post(null, sdf.parse("25/10/2021"), "LaMelo Ball is compared to Magic Johnson.", "I really think that is wrong to compare players from different generation, but Melo has the magic too.", alex);
-
         userRepository.saveAll(Arrays.asList(maria, alex, jack));
+
+        Post post1 = new Post(null, sdf.parse("26/10/2021"), "Kyrie Irving doesn't want to vaccinate.", "Shame on Kyrie, his selfishness attitude is damaging his team, Brooklyn Nets, to get any chance of going to play-offs this season.", new AuthorDTO(alex));
+        Post post2 = new Post(null, sdf.parse("25/10/2021"), "LaMelo Ball is compared to Magic Johnson.", "I really think that is wrong to compare players from different generation, but Melo has the magic too.", new AuthorDTO(alex));
+
+        
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        alex.getPosts().addAll(Arrays.asList(post1, post2));
+
+        userRepository.save(alex);
     }
     
 }
